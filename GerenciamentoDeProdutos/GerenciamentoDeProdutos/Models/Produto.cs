657f51bb636b1c599ad3886cfc5ccb2df34ba0fd
Kamila Luz <146234680+kamilaluz@users.noreply.github.com>
2024-02-24 15:07:13 -0300

    Criada a classe Produto

---
+++ GerenciamentoDeProdutos/GerenciamentoDeProdutos/Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GerenciamentoDeProdutos.Models
+{
+    public class Produto
+    {
+        public int Id;
+        [Required(ErrorMessage ="Informe o nome do produto")]
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Informe o preço do produto")]
+        public double Preco {  get; set; }
+
+        [Required(ErrorMessage = "Informe a quantidade em estoque")]
+        public int QuantidadeEstoque { get; set; }
+
+    }
+}
